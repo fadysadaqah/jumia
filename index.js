3551e4f087a6610b71d774ba8f2d498b46ebee88
Fady Sadaqah <fadysadaqah@gmail.com>
2020-06-21 12:06:09 +0200

    fdsdf

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var pages = 0 ;
 setInterval(() => {
     pages+=1
     $('.pages').text(pages)
-    if (pages==3675){
+    if (pages==4000){
         $('.candles').text(1)
+        $('.code').prop('hidden',false)
     }
 }, 500);
